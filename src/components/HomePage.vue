5687a3ef1e86efe66c0c3eb552714f277e1c29bb
Zheliu46 <3992285726@qq.com>
2025-03-22 09:35:28 +0800

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+  <body>
+    <h1 :class="title">标题待定<img src="" alt=""></h1>
+    <SearchBox />
+    <div class="shortcut">
+      <a class="cut" href="https://www.bilibili.com/"><img width="17%" src="@/assets/bilibili.ico" alt=""></a>
+      <a class="cut" href=""><img src="" alt=""></a>
+      <a class="cut" href=""></a>
+      <a class="cut" href=""></a>
+    </div>
+  </body>
+
+</template>
+
+<style>
+@import'@/assets/HomePage.css';
+</style>
+
+<script>
+import SearchBox from './SearchBox.vue';
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    SearchBox
+  },
+
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+      title: "title",
+    }
+  },
+
+  methods: {
+  }
+
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: 'ws://0.0.0.0:8080/ws'
+    },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('ico')
+      .test(/\.ico$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: '[name].[ext]',
+        outputPath: 'assets/' // 输出目录
+      })
+      .end();
+  }
 })
